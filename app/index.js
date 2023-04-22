339f0a78a727bfc28e944ba19243dd82bea56884
Vickie Do <dovh@uci.edu>
2023-04-22 16:32:46 -0700

    added card switching

--- app/index.js
+++ app/index.js
@@ -2,9 +2,33 @@ import { StyleSheet, Text, View, Image, ScrollView} from "react-native";
 import { Link, Stack } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons'; 
 import CardFront from "../components/CardFront";
+import CardBack from "../components/CardBack";
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 
 export default function Page() {
+  const rotate = useSharedValue(0);
+  const frontAnimatedStyles = useAnimatedStyle(()=>{
+    const rotateValue = interpolate(rotate.value,[0,1],[0,180])
+    return{
+      transform:[
+        {
+          rotateY : withTiming(`${rotateValue}deg`,{duration:1000})
+        }
+      ]
+    }
+  })
+  const backAnimatedStyles = useAnimatedStyle(()=>{
+    const rotateValue = interpolate(rotate.value,[0,1],[180,360])
+    return{
+      transform:[
+        {
+          rotateY : withTiming(`${rotateValue}deg`,{duration:1000})
+        }
+      ]
+    }
+  })
+  
   return (
     <View style={styles.container}> 
       <ScrollView 
@@ -72,7 +96,16 @@ export default function Page() {
         </Text>
       </View>
 
-       <CardFront/>       
+      <View style={styles.containerFlip}>
+        <View>
+          <Animated.View style={[styles.frontcard,frontAnimatedStyles]}>
+            <CardFront rotate={rotate}  />
+          </Animated.View>
+          <Animated.View style={[styles.backCard,backAnimatedStyles]}>
+            <CardBack rotate={rotate} />
+          </Animated.View>
+        </View>
+      </View>
       {/* <View style={styles.checkingcard}>
         <View style={styles.imagePos}>
           <Image
@@ -271,5 +304,26 @@ const styles = StyleSheet.create({
   numberPos: {
     marginRight: 20,
     marginBottom: 25,
-  }
+  },
+
+  containerFlip: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ frontcard:{
+  position:"absolute",
+  backfaceVisibility:'hidden'
+ },
+ backCard:{
+  backfaceVisibility:"hidden"
+ },
+  button:{
+    paddingHorizontal:25,
+    paddingVertical:5,
+    backgroundColor:"#8ecae6",
+    marginTop:10,
+    borderRadius:5,
+  },
 });
