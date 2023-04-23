6466b727b57439644946415e4c7074b53d221db0
Eugene <75242911+EugenSong@users.noreply.github.com>
2023-04-23 02:03:52 -0700

    eugene's pr w/ dropdown

---
+++ components/dropdown.tsx
@@ -0,0 +1,43 @@
+import DropDownPicker from "react-native-dropdown-picker";
+import React from "react";
+import { useState } from "react";
+import { TranslatorProvider } from "react-native-translator";
+import { View } from "react-native/types";
+
+const Dropdown = ({ setLanguage }) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+
+  const [languages, setInitialLangs] = useState([
+    {
+      label: "English",
+      value: "english",
+    },
+    {
+      label: "Korean",
+      value: "korean",
+    },
+    {
+      label: "Japanese",
+      value: "japanese",
+    },
+  ]);
+
+
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={languages}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setInitialLangs}
+      onSelectItem={(selectedLanguage) => {
+        console.log(selectedLanguage);
+        setLanguage(selectedLanguage.value);
+      }}
+    />
+  );
+};
+
+export default Dropdown;
